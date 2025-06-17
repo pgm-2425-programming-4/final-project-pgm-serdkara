1f686761ed91bbc20ae9edd2198cbb212821798e
Serdar Karaman <serdar.karaman@outlook.com>
2025-06-17 15:21:26 +0200

    add initial project structure with Strapi and React integration

---
+++ client/my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
+import { PaginatedTaskList } from "./components/app/paginated-task-list/paginated-task-list";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div style={{ margin: "2rem" }}>
+        <PaginatedTaskList />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
