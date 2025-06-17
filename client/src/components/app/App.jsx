bd4e1317c82fd2176af9052aaab3d51ca510cc17
Serdar Karaman <serdar.karaman@outlook.com>
2025-06-17 20:33:46 +0200

    Clean working version

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./components/app/app.css";
 import { PaginatedTaskList } from "./paginated-task-list/paginated-task-list";
 
 const queryClient = new QueryClient();
